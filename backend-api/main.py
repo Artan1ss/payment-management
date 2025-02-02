eb5a8e1f561e7cf62cdfecc807181ca02e8ced34
Artan1ss <xuefeng0580@gmail.com>
2025-02-02 01:15:08 -0500

    Enabled CORS for frontend

--- backend-api/main.py
+++ backend-api/main.py
@@ -28,9 +28,14 @@ class PaymentUpdateModel(BaseModel):
     payee_due_date: str 
     due_amount: float
 
+origins = [
+    "https://payment-management-omega.vercel.app",  
+    "http://localhost:4200",  
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://payment-management-omega.vercel.app"],  
+    allow_origins=origins,  
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"], 
